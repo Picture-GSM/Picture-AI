61fc2c1202713edec6132e76b9696ab69404df92
sinmugyeol <tlsanruf123@gmail.com>
2022-03-23 18:08:08 +0900

    [UPDATE] plt.imread -> Image.open

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,6 @@
 import os
+from os import listdir
+from os.path import join
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
@@ -8,98 +10,25 @@ from skimage.transform import resize
 
 
 class Dataset(Dataset):
-    def __init__(self, path, transform=None):
+    def __init__(self, path, transform=None, style=None):
         super(Dataset, self).__init__()
         self.path = path
-        if transform:
-            self.files = [x for x in os.listdir(os.path.join(path, 'style'))]
+        self.style = style
+        if self.style:
+            self.path = self.path + '/style'
+            self.files = [join(self.path, x) for x in listdir(self.path)]
+            print(self.path)
         else:
-            self.files = [x for x in os.listdir(os.path.join(path, 'content'))]
+            self.path = self.path + '/content'
+            self.files = [join(self.path, x) for x in listdir(self.path)]
+            print(self.path)
         self.transform = transform
-        self.totensor = ToTensor()
 
     def __len__(self):
         return len(self.files)
 
     def __getitem__(self, index):
-        if self.transform:
-            img = plt.imread(os.path.join(self.path + '/style', self.files[index]))
-            img = self.transform(img)
-        else:
-            img = plt.imread(os.path.join(self.path + '/content', self.files[index]))
-
-        img = self.totensor(img)
+        img = Image.open(self.files[index]).convert('RGB')
+        img = self.transform(img)
 
         return img
-
-
-class ToTensor(object):
-    def __call__(self, data):
-        for key, value in data.items():
-            value = value.transpose((2, 0, 1)).astype(np.float32)
-            data[key] = torch.from_numpy(value)
-
-        return data
-
-
-class Normalization(object):
-    def __init__(self, mean=0.5, std=0.5):
-        self.mean = mean
-        self.std = std
-
-    def __call__(self, data):
-        for key, value in data.items():
-            data[key] = (value - self.mean) / self.std
-
-        return data
-
-
-class CenterCrop(object):
-    def __init__(self, shape):
-        self.shape = shape
-
-    def __call__(self, data):
-        h, w = data['label'].shape[:2]
-        new_h, new_w = self.shape
-
-        top = (h - new_h) // 2
-        left = (w - new_w) // 2
-
-        id_y = np.arange(top, top + new_h, 1)[:, np.newaxis]
-        id_x = np.arange(left, left + new_w, 1)
-
-        for key, value in data.items():
-            data[key] = value[id_y, id_x]
-
-        return data
-
-
-class RandomCrop(object):
-    def __init__(self, shape):
-        self.shape = shape
-
-    def __call__(self, data):
-        h, w = data['label'].shape[:2]
-        new_h, new_w = self.shape
-
-        top = np.random.randint(0, h - new_h)
-        left = np.random.randint(0, w - new_w)
-
-        id_y = np.arange(top, top + new_h, 1)[:, np.newaxis]
-        id_x = np.arange(left, left + new_w, 1)
-
-        for key, value in data.items():
-            data[key] = value[id_y, id_x]
-
-        return data
-
-
-class Resize(object):
-    def __init__(self, shape):
-        self.shape = shape
-
-    def __call__(self, data):
-        for key, value in data.items():
-            data[key] = resize(value, output_shape=(self.shape[0], self.shape[1], self.shape[2]))
-
-        return data

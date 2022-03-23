1a895adfd0ff7d8e6969e81a8d0a0ec9540d83a5
sinmugyeol <tlsanruf123@gmail.com>
2022-03-23 18:06:57 +0900

    [UPDATE] reshape -> view

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import numpy as np
-
+import torch
 
 def calc_mean_std(feat, eps=1e-5):
     n, c, h, w = feat.shape
-    feat_var = np.var(feat.reshape(n, c, -1), axis=2) + eps
-    feat_std = np.sqrt(feat_var).reshape(n, c, 1, 1)
-    feat_mean = np.mean(feat.reshape(n, c, -1), axis=2).reshape(n, c, 1, 1)
+    feat_var = feat.view(n, c, -1).var(dim=2) + eps
+    feat_std = feat_var.sqrt().view(n, c, 1, 1)
+    feat_mean = feat.view(n, c, -1).mean(dim=2).view(n, c, 1, 1)
 
     return feat_mean, feat_std
